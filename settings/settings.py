c88cdc91031c9706517f1632f059523acfe4782f
Mrtc101 <martincogo4@gmail.com>
2023-06-20 19:53:02 -0300

    Modelos de TaslManager y PeopleManager

--- settings/settings.py
+++ settings/settings.py
@@ -37,6 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django_extensions',
+    'PeopleManager',
+    'TaskManager'
 ]
 
 MIDDLEWARE = [
@@ -80,7 +83,6 @@ DATABASES = {
     }
 }
 
-
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
 
@@ -103,7 +105,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/4.2/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'es'
 
 TIME_ZONE = 'UTC'
 
