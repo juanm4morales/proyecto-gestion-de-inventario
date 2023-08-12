1a60bfc2aa9e44cd6335061424c521032d7673b1
Mrtc101 <martincogo4@gmail.com>
2023-08-12 17:40:25 -0300

    creating client with vite

--- django_crud_api/settings.py
+++ django_crud_api/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-*2@_(fi9=&nyg%ghe8=cy7db_ytf0hj1*+%_mfkl%q3$swch=-
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["localhost"]
 
 
 # Application definition
