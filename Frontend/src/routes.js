d6645b354f6071288789982ca56aae7cc024f685
Mrtc101 <martincogo4@gmail.com>
2023-08-15 22:32:17 -0300

    Proyecto Re-estructurado

---
+++ Frontend/src/routes.js
@@ -0,0 +1,39 @@
+/*!
+
+=========================================================
+* Paper Dashboard React - v1.3.2
+=========================================================
+
+* Product Page: https://www.creative-tim.com/product/paper-dashboard-react
+* Copyright 2023 Creative Tim (https://www.creative-tim.com)
+
+* Licensed under MIT (https://github.com/creativetimofficial/paper-dashboard-react/blob/main/LICENSE.md)
+
+* Coded by Creative Tim
+
+=========================================================
+
+* The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+
+*/
+import InventarioForm from "views/InventarioForm";
+import InventarioList from "views/InventarioPage.js";
+
+var routes = [
+  {
+    path: "/Inventario",
+    name: "Inventario",
+    icon: "nc-icon nc-tile-56",
+    component: <InventarioList />,
+    layout: "/admin",
+  },
+  {
+    path: "/InventarioForm",
+    name: "InventarioForm",
+    icon: "",
+    component: <InventarioForm />,
+    layout: "/admin",
+  },
+];
+
+export default routes;
