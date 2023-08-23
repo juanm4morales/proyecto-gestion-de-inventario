6ea81dd217e5ce37423536bfa4c0154eecc268a2
emiliano-f <def.emiliano@gmail.com>
2023-08-22 22:23:27 -0300

    Deteled PATCH and DELETE http methods

--- Backend/inventario/views.py
+++ Backend/inventario/views.py
@@ -2,30 +2,33 @@ from rest_framework import viewsets
 from . import serializer
 from . import models
 
-class TipoInsumoCRUD(viewsets.ModelViewSet):
+class CustomModelViewSet(viewsets.ModelViewSet):
+    http_method_names = ['post', 'get', 'put']
+
+class TipoInsumoCRUD(CustomModelViewSet):
     serializer_class = serializer.TipoInsumoSerializer
     queryset = models.TipoInsumo.objects.all()
 
-class InsumoCRUD(viewsets.ModelViewSet):
+class InsumoCRUD(CustomModelViewSet):
     serializer_class = serializer.InsumoSerializer
     queryset = models.Insumo.objects.all()
 
-class TipoHerramientaCRUD(viewsets.ModelViewSet):
+class TipoHerramientaCRUD(CustomModelViewSet):
     serializer_class = serializer.TipoHerramientaSerializer
     queryset = models.TipoHerramienta.objects.all()
 
-class HerramientaCRUD(viewsets.ModelViewSet):
+class HerramientaCRUD(CustomModelViewSet):
     serializer_class = serializer.HerramientaSerializer
     queryset = models.Herramienta.objects.all()
 
-class OrdenRetiroCRUD(viewsets.ModelViewSet):
+class OrdenRetiroCRUD(CustomModelViewSet):
     serializer_class = serializer.OrdenRetiroSerializer
     queryset = models.OrdenRetiro.objects.all()
 
-class AjusteStockCRUD(viewsets.ModelViewSet):
+class AjusteStockCRUD(CustomModelViewSet):
     serializer_class = serializer.AjusteStockSerializer
     queryset = models.AjusteStock.objects.all()
 
-class EstadoHerramientaCRUD(viewsets.ModelViewSet):
+class EstadoHerramientaCRUD(CustomModelViewSet):
     serializer_class = serializer.EstadoHerramientaSerializer
     queryset = models.EstadoHerramienta.objects.all()
